99799d9d6e67c5e06ce01815a26005512e8ad992
d.kovalenko <d.kovalenko@postgrespro.ru>
2025-08-26 11:11:03 +0300

    cleanup

--- init_test_env.sh
+++
@@ -1,5 +0,0 @@
-export PG_BIN='/home/dima/MY/Work/CurrentTasks/2025/PROBACKUP3/2025_03_25/Servers/n001c/debug/ent-17/install/bin'
-export PG_CONFIG=${PG_BIN}/pg_config
-export PGPROBACKUPBIN="/home/dima/MY/GitLab2/pg_probackup2/ee_2_8/work01/pg_probackup"
-export PATH=${PATH}:${PG_BIN}
-
