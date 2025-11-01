62e8b12699e676ea757d600d41dcfb8fa43d08be
d.kovalenko <d.kovalenko@postgrespro.ru>
2025-11-01 15:37:33 +0300

    setup.py is updated (readme)
    +formatting

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ try:
 except ImportError:
     from distutils.core import setup
 
+# Get contents of README file
+with open("README.md", "r") as f:
+    readme = f.read()
+
 setup(
     version="0.0.2",
     name="testgres.common",
@@ -12,10 +16,10 @@ setup(
     package_dir={"testgres.common": "src"},
     description='Testgres common code',
     url='https://github.com/postgrespro/testgres.common',
+    long_description=readme,
     long_description_content_type='text/markdown',
     license='PostgreSQL',
     author='Postgres Professional',
     author_email='testgres@postgrespro.ru',
     keywords=['testgres'],
 )
-
